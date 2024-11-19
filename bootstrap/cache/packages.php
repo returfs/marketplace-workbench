95081f275381e944bc82a271924f47173c469c79
badasukerubin <kelvinvip8@gmail.com>
2024-11-19 23:41:41 +0100

    initial commit

---
+++ bootstrap/cache/packages.php
@@ -0,0 +1,147 @@
+<?php return array (
+  'barryvdh/laravel-ide-helper' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Barryvdh\\LaravelIdeHelper\\IdeHelperServiceProvider',
+    ),
+  ),
+  'jenssegers/agent' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Jenssegers\\Agent\\AgentServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Agent' => 'Jenssegers\\Agent\\Facades\\Agent',
+    ),
+  ),
+  'laravel/fortify' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Fortify\\FortifyServiceProvider',
+    ),
+  ),
+  'laravel/jetstream' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Jetstream\\JetstreamServiceProvider',
+    ),
+  ),
+  'laravel/sanctum' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Sanctum\\SanctumServiceProvider',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'lorisleiva/laravel-actions' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Lorisleiva\\Actions\\ActionServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Action' => 'Lorisleiva\\Actions\\Facades\\Actions',
+    ),
+  ),
+  'lorisleiva/lody' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Lorisleiva\\Lody\\LodyServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Lody' => 'Lorisleiva\\Lody\\Lody',
+    ),
+  ),
+  'nesbot/carbon' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Carbon\\Laravel\\ServiceProvider',
+    ),
+  ),
+  'nunomaduro/collision' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
+    ),
+  ),
+  'nunomaduro/termwind' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Termwind\\Laravel\\TermwindServiceProvider',
+    ),
+  ),
+  'orchestra/canvas' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Orchestra\\Canvas\\LaravelServiceProvider',
+    ),
+  ),
+  'orchestra/canvas-core' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Orchestra\\Canvas\\Core\\LaravelServiceProvider',
+    ),
+  ),
+  'pestphp/pest-plugin-laravel' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Pest\\Laravel\\PestServiceProvider',
+    ),
+  ),
+  'returfs/shared-internal' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Returfs\\Marketplace\\Shared\\Internal\\SharedInternalServiceProvider',
+    ),
+  ),
+  'spatie/laravel-data' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Spatie\\LaravelData\\LaravelDataServiceProvider',
+    ),
+  ),
+  'spatie/laravel-ray' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Spatie\\LaravelRay\\RayServiceProvider',
+    ),
+  ),
+  'spatie/php-structure-discoverer' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Spatie\\StructureDiscoverer\\StructureDiscovererServiceProvider',
+    ),
+  ),
+  'returfs/item-create-new' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Returfs\\Marketplace\\Internal\\ItemCreateNew\\ItemCreateNewServiceProvider',
+    ),
+  ),
+);
